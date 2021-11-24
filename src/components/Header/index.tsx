ed37f4727eba67c8479c0fdb559a0da7c2ece7d6
Sheila Kerber <sheila.skerber@gmail.com>
2021-11-24 09:32:45 -0300

    creates modal using the library react-modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,36 @@
+import { useState } from 'react'
 import logoImg from '../../assets/logo.svg'
 import { Container, Content } from './styles'
- 
+import Modal from 'react-modal' 
 
 export function Header(){
+
+    // configurando o modal da aplicacao [button 'Nova transacao']
+    const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
+
+    function handleOpenNewTransactionModal(){  //handle = interacao usuario (ex: click)
+        setIsNewTransactionModalOpen(true)
+    }
+
+    function handleCloseNewTransactionModal(){
+        setIsNewTransactionModalOpen(false)
+    }
+
     return (
         <Container>
             <Content>
                 <img src={logoImg} alt="dt money" />
-                <button type="button">
+                <button type="button" onClick={handleOpenNewTransactionModal}>
                     Nova transação
                 </button>
+                
+                <Modal
+                    isOpen={isNewTransactionModalOpen}
+                    onRequestClose={handleCloseNewTransactionModal}
+                >
+                    
+                    <h2>Cadastrar transacao! </h2>
+                </Modal>
             </Content>
         </Container>
     )
