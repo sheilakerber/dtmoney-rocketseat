bf1b9afe95004572001442d0bc254b093899f5bd
Sheila Kerber <sheila.skerber@gmail.com>
2021-11-24 10:09:22 -0300

    removes Modal from Header and add it to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import { GlobalStyle } from "./styles/globals";
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
+import Modal from 'react-modal'
+import { useState } from "react";
+
+Modal.setAppElement('#root')    // por questoes de acessibilidade, para falar qual eh o elemento root do modal
 
 export function App() {
+  // configurando o modal da aplicacao [button 'Nova transacao']
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
+
+  function handleOpenNewTransactionModal(){  //handle = interacao usuario (ex: click)
+      setIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal(){
+      setIsNewTransactionModalOpen(false)
+  }
+
   return (
     <>
-      <Header/>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+
+      <Modal
+                    isOpen={isNewTransactionModalOpen}
+                    onRequestClose={handleCloseNewTransactionModal}
+                >  
+                    <h2>Cadastrar transacao! </h2>
+                </Modal>
+
+
       <GlobalStyle />
     </>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,34 +3,21 @@ import logoImg from '../../assets/logo.svg'
 import { Container, Content } from './styles'
 import Modal from 'react-modal' 
 
-export function Header(){
+interface HeaderProps {
+    onOpenNewTransactionModal: () => void;
+}
 
-    // configurando o modal da aplicacao [button 'Nova transacao']
-    const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
-
-    function handleOpenNewTransactionModal(){  //handle = interacao usuario (ex: click)
-        setIsNewTransactionModalOpen(true)
-    }
-
-    function handleCloseNewTransactionModal(){
-        setIsNewTransactionModalOpen(false)
-    }
+export function Header( { onOpenNewTransactionModal}: HeaderProps){
 
     return (
         <Container>
             <Content>
                 <img src={logoImg} alt="dt money" />
-                <button type="button" onClick={handleOpenNewTransactionModal}>
+                <button type="button" onClick={onOpenNewTransactionModal}>
                     Nova transação
                 </button>
+
                 
-                <Modal
-                    isOpen={isNewTransactionModalOpen}
-                    onRequestClose={handleCloseNewTransactionModal}
-                >
-                    
-                    <h2>Cadastrar transacao! </h2>
-                </Modal>
             </Content>
         </Container>
     )
