56bfc80a7c3b7deb4767bb041ceb2e9ae44e2f87
Sheila Kerber <sheila.skerber@gmail.com>
2021-11-24 14:33:33 -0300

    adds button with img to close modal window

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,9 @@
-import { prependOnceListener } from 'process'
+// import { prependOnceListener } from 'process'
 import Modal  from 'react-modal'
+
+import closeImg from '../../assets/close.svg'
+
+
 import { Container } from './styles'
 
 interface NewTransactionModalProps{
@@ -15,8 +19,17 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
             overlayClassName="react-modal-overlay"
             className="react-modal-content"
         >  
+
+            <button 
+                type="button"
+                onClick={onRequestClose}
+                className="react-modal-close"
+            >
+                <img src={closeImg} alt="Fechar modal" />
+            </button>
+
             <Container>
-                <h2>Cadastrar transação! </h2>
+                <h2>Cadastrar transação</h2>
 
                 <input
                     placeholder="Título"
